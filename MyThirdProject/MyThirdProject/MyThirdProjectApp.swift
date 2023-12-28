fa9161bd3f89ae1d120d0d09a6d7c4209d9b83af
Alex Dmitriev <107206552+Carbofoss@users.noreply.github.com>
2023-12-29 00:16:19 +0300

    Add files via upload
    Написать вывод сохранений и возможность их удаления

---
+++ MyThirdProject/MyThirdProject/MyThirdProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyThirdProjectApp.swift
+//  MyThirdProject
+//
+//  Created by MacBook on 03.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyThirdProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
